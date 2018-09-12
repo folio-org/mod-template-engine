78b7007e7cf7d61cb2e96ead60699e26f136c010
oleksii-popov <oleksii_popov@epam.com>
2018-09-12 18:30:17 +0300

    MODLOGIN-56 Add CRUD REST to mod-template-engine Added descriptor Added RAML files Added db schema Implemented POST method

--- src/main/java/org/folio/rest/impl/TemplateResourceImpl.java
+++ src/main/java/org/folio/rest/impl/TemplateResourceImpl.java
@@ -2,51 +2,64 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import static io.vertx.core.Future.succeededFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import org.folio.rest.jaxrs.resource.TemplateResource;
-import org.folio.rest.jaxrs.model.Tag;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.folio.rest.RestVerticle;
+import org.folio.rest.jaxrs.model.TemplateJson;
+import org.folio.rest.jaxrs.resource.TemplateResource;
+import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.utils.TenantTool;
+import org.folio.rest.tools.utils.ValidationHelper;
+
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.UUID;
+
+import static io.vertx.core.Future.succeededFuture;
 
 public class TemplateResourceImpl implements TemplateResource {
 
   private static final String TEMPLATE_STUB_PATH = "ramls/examples/template.sample";
   private static final String CONTENT_TYPE_HEADER = "Content-Type";
   private static final String APPLICATION_JSON = "application/json";
+  private static final String TEMPLATES_TABLE = "template";
+  private final Logger logger = LoggerFactory.getLogger("mod-template-engine");
 
   @Override
   public void postTemplate(
-                       String template,
-                       String description,
-                       String lang,
-                       Map<String, String> okapiHeaders,
-                       Handler<AsyncResult<Response>> asyncResultHandler,
-                       Context context) {
+    TemplateJson entity,
+    Map<String, String> okapiHeaders,
+    Handler<AsyncResult<Response>> asyncResultHandler,
+    Context context) {
 
-    String tenantId = TenantTool.calculateTenantId(
-      okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
-    String id = entity.getId();
-    if (id == null || id.isEmpty()) {
-      id = UUID.randomUUID().toString();
-      entity.setId(id);
-    }
-    PostgresClient.getInstance(context.owner(), tenantId).save(TAGS_TABLE,
-      id, entity,
-      reply -> {
-        if (reply.succeeded()) {
-          String ret = reply.result();
-          entity.setId(ret);
-          asyncResultHandler.handle(succeededFuture(PostTagsResponse.
-            respond201WithApplicationJson(entity, PostTagsResponse.headersFor201()
-              .withLocation(LOCATION_PREFIX + ret))));
-        } else {
-          ValidationHelper.handleError(reply.cause(), asyncResultHandler);
-        }
-      });
+    context.runOnContext(h -> {
+      try {
+        String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+        String id = UUID.randomUUID().toString();
+        entity.setId(id);
+
+        PostgresClient.getInstance(context.owner(), tenantId).save(TEMPLATES_TABLE,
+          id, entity,
+          reply -> {
+            if (reply.succeeded()) {
+              logger.info("Template created with id: " + id);
+              asyncResultHandler.handle(Future.succeededFuture(PostTemplateResponse.withJsonCreated(entity)));
+            } else {
+              logger.error("Template creation error.");
+              ValidationHelper.handleError(reply.cause(), asyncResultHandler);
+            }
+          });
+      } catch (Exception e) {
+        String message = "Template creation internal error";
+        logger.error(message, e);
+        asyncResultHandler.handle(Future.succeededFuture(
+          PostTemplateResponse.withPlainInternalServerError(message)));
+      }
+    });
   }
 
   @Override
