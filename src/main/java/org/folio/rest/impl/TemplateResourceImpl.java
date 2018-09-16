35c980000ae3fd94bc4fc84ede1540d10c30ccc0
OleksiiKuzminov <Oleksii_Kuzminov@epam.com>
2018-09-17 02:02:32 +0300

    add put and get crud operations

--- src/main/java/org/folio/rest/impl/TemplateResourceImpl.java
+++ src/main/java/org/folio/rest/impl/TemplateResourceImpl.java
@@ -9,13 +9,21 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import org.folio.rest.RestVerticle;
 import org.folio.rest.jaxrs.model.TemplateJson;
+import org.folio.rest.jaxrs.model.TemplatesCollectionJson;
 import org.folio.rest.jaxrs.resource.TemplateResource;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
 import org.folio.rest.tools.utils.TenantTool;
 import org.folio.rest.tools.utils.ValidationHelper;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -27,6 +35,8 @@ public class TemplateResourceImpl implements TemplateResource {
   private static final String CONTENT_TYPE_HEADER = "Content-Type";
   private static final String APPLICATION_JSON = "application/json";
   private static final String TEMPLATES_TABLE = "template";
+  private static final String TEMPLATES_ID_FIELD = "'id'";
+  public static final String INTERNAL_ERROR = "Internal Server error";
   private final Logger logger = LoggerFactory.getLogger("mod-template-engine");
 
   @Override
@@ -38,7 +48,7 @@ public class TemplateResourceImpl implements TemplateResource {
 
     vertxContext.runOnContext(h -> {
       try {
-        String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+        String tenantId = getTenant(okapiHeaders);
         String id = UUID.randomUUID().toString();
         entity.setId(id);
 
@@ -62,6 +72,46 @@ public class TemplateResourceImpl implements TemplateResource {
   }
 
   @Override
+  public void getTemplate(int length, int start, String sortBy,
+                          String query, Map<String, String> okapiHeaders,
+                          Handler<AsyncResult<Response>> asyncResultHandler,
+                          Context vertxContext) throws Exception {
+    try {
+      vertxContext.runOnContext(v -> {
+        String tenantId = getTenant(okapiHeaders);
+        String[] fieldList = {"*"};
+        try {
+          CQLWrapper cql = getCQL(query, length, start - 1);
+          PostgresClient.getInstance(vertxContext.owner(), tenantId).get(
+            TEMPLATES_TABLE, TemplateJson.class, fieldList, cql, true, false, getReply -> {
+              if(getReply.failed()) {
+                logger.debug("Error in PostgresClient get operation " + getReply.cause().getLocalizedMessage());
+                asyncResultHandler.handle(Future.succeededFuture(GetTemplateResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+              } else {
+                TemplatesCollectionJson templatesCollection = new TemplatesCollectionJson();
+                List<TemplateJson> templateJsonList = (List<TemplateJson>)getReply.result().getResults();
+                templatesCollection.setTemplates(templateJsonList);
+                templatesCollection.setTotalRecords(getReply.result().getResultInfo().getTotalRecords());
+                asyncResultHandler.handle(Future.succeededFuture(GetTemplateResponse.withJsonOK(templatesCollection)));
+              }
+            });
+        } catch(Exception e) {
+          logger.debug("Error invoking Postgresclient: "+ e.getLocalizedMessage());
+          asyncResultHandler.handle(Future.succeededFuture(GetTemplateResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+        }
+      });
+    } catch(Exception e) {
+      logger.debug("Error running on vertx context: " + e.getLocalizedMessage());
+      if(e.getCause() != null && e.getCause().getClass().getSimpleName().contains("CQLParseException")) {
+        asyncResultHandler.handle(Future.succeededFuture(GetTemplateResponse.withPlainBadRequest("CQL Parsing Error for '" + query + "': " +
+          e.getLocalizedMessage())));
+      } else {
+        asyncResultHandler.handle(Future.succeededFuture(GetTemplateResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+      }
+    }
+  }
+
+  @Override
   public void getTemplateByTemplateId(@NotNull String templateId, Map<String, String> okapiHeaders,
                                       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     //TODO replace stub response
@@ -80,6 +130,54 @@ public class TemplateResourceImpl implements TemplateResource {
   }
 
   @Override
+  public void putTemplateByTemplateId(@NotNull String templateId, TemplateJson entity, Map<String, String> okapiHeaders,
+                                      Handler<AsyncResult<Response>> asyncResultHandler,
+                                      Context vertxContext) throws Exception {
+    try {
+      vertxContext.runOnContext(v -> {
+        String tenantId = getTenant(okapiHeaders);
+        Criteria idCrit = new Criteria();
+        idCrit.addField(TEMPLATES_ID_FIELD);
+        idCrit.setOperation("=");
+        idCrit.setValue(templateId);
+        try {
+          PostgresClient.getInstance(vertxContext.owner(), tenantId).get(TEMPLATES_TABLE, TemplateJson.class, new Criterion(idCrit),true, getReply -> {
+            if(getReply.failed()) {
+              logger.debug("PostgresClient get operation failed: " + getReply.cause().getLocalizedMessage());
+              asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+            } else {
+              List<TemplateJson> templateList = (List<TemplateJson>)getReply.result().getResults();
+              if(templateList.isEmpty()) {
+                asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainNotFound("No templates was found")));
+              } else {
+                try {
+                  PostgresClient.getInstance(vertxContext.owner(), tenantId).update(TEMPLATES_TABLE, entity, new Criterion(idCrit), true, putReply -> {
+                    if(putReply.failed()) {
+                      logger.debug("Error with PostgresClient update operation: " + putReply.cause().getLocalizedMessage());
+                      asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+                    } else {
+                      asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withJsonOK(entity)));
+                    }
+                  });
+                } catch(Exception e) {
+                  logger.debug("Error with PostgresClient: " + e.getLocalizedMessage());
+                  asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+                }
+              }
+            }
+          });
+        } catch(Exception e) {
+          logger.debug("Error with PostgresClient: " + e.getLocalizedMessage());
+          asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+        }
+      });
+    } catch(Exception e) {
+      logger.debug("Error running on vertx context: " + e.getLocalizedMessage());
+      asyncResultHandler.handle(Future.succeededFuture(PutTemplateByTemplateIdResponse.withPlainInternalServerError(INTERNAL_ERROR)));
+    }
+  }
+
+  @Override
   public void deleteTemplateByTemplateId(@NotNull String templateId,
                                          Map<String, String> okapiHeaders,
                                          Handler<AsyncResult<Response>> asyncResultHandler,
@@ -87,7 +185,7 @@ public class TemplateResourceImpl implements TemplateResource {
 
     vertxContext.runOnContext(h -> {
       try {
-        String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+        String tenantId = getTenant(okapiHeaders);
 
         PostgresClient
           .getInstance(vertxContext.owner(), tenantId)
@@ -117,5 +215,17 @@ public class TemplateResourceImpl implements TemplateResource {
           PostTemplateResponse.withPlainInternalServerError(message)));
       }
     });
+  }
+
+  private String getTenant(Map<String, String> headers) {
+    return TenantTool.calculateTenantId(headers.get(RestVerticle.OKAPI_HEADER_TENANT));
+  }
+
+  private CQLWrapper getCQL(String query, int limit, int offset)
+    throws org.z3950.zing.cql.cql2pgjson.FieldException {
+    CQL2PgJSON cql2pgJson = new CQL2PgJSON(TEMPLATES_TABLE + ".jsonb");
+    return new CQLWrapper(cql2pgJson, query)
+      .setLimit(new Limit(limit))
+      .setOffset(new Offset(offset));
   }
 }
