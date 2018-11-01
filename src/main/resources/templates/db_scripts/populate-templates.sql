b40e1d157f32730fd57e0ef87befcad4a2a64931
Dmytro Tur <dmytro_tur@epam.com>
2018-11-01 15:21:29 +0200

    Escape body

--- src/main/resources/templates/db_scripts/populate-templates.sql
+++ src/main/resources/templates/db_scripts/populate-templates.sql
@@ -10,7 +10,7 @@ INSERT INTO template (_id, jsonb) VALUES
  "localizedTemplates": {
    "en": {
      "header": "Your Folio password changed",
-     "body": "Dear {{user.firstName}},\n\nYour password has been changed.\nThis is a confirmation that your password was changed on {{dateTime}}.\n\nDid not change your password? Contact your Folio System Administrator to help secure your account.\n\nRegards,\n\nFolio Support"
+     "body": "Dear {{user.firstName}},<\/br><\/br>\r\n\r\nYour password has been changed.<\/br>\r\nThis is a confirmation that your password was changed on {{dateTime}}.<\/br><\/br>\r\n\r\nDid not change your password? Contact your Folio System Administrator to help secure your account.<\/br><\/br>\r\n\t\r\nRegards,<\/br><\/br>\r\n\r\nFolio Support"
    }
  }
 }') ON CONFLICT DO NOTHING;
