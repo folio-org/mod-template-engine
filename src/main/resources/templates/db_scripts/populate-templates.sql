a255271fe805faae03977c32e2998ad6afc9f5ec
Dmytro Tur <dmytro_tur@epam.com>
2018-11-05 12:13:09 +0200

    Moving to MIME type

--- src/main/resources/templates/db_scripts/populate-templates.sql
+++ src/main/resources/templates/db_scripts/populate-templates.sql
@@ -3,13 +3,13 @@ INSERT INTO template (_id, jsonb) VALUES
  "id": "0ff6678f-53cd-4a32-9937-504c28f14077",
  "description": "Template for password changed email",
  "outputFormats": [
-   "html"
+   "text/plain"
  ],
  "templateResolver": "mustache",
  "localizedTemplates": {
    "en": {
      "header": "Your Folio password changed",
-     "body": "Dear {{user.firstName}},<\/br><\/br>\r\n\r\nYour password has been changed.<\/br>\r\nThis is a confirmation that your password was changed on {{dateTime}}.<\/br><\/br>\r\n\r\nDid not change your password? Contact your Folio System Administrator to help secure your account.<\/br><\/br>\r\n\t\r\nRegards,<\/br><\/br>\r\n\r\nFolio Support"
+     "body": "Dear {{user.firstName}},\n\nYour password has been changed.\nThis is a confirmation that your password was changed on {{dateTime}}.\n\nDid not change your password? Contact your Folio System Administrator to help secure your account.\n\t\nRegards,\n\nFolio Support"
    }
  }
 }') ON CONFLICT DO NOTHING;
