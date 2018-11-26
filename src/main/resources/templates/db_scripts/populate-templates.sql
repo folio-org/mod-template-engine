6d520bb9782a2caafa834b07c694cdae627873dd
Dmytro Tur <dmytro_tur@epam.com>
2018-11-26 17:52:42 +0200

    UIU-511 UIU-512 Create password email and reset password email templates added to pre-populate script

--- src/main/resources/templates/db_scripts/populate-templates.sql
+++ src/main/resources/templates/db_scripts/populate-templates.sql
@@ -12,4 +12,32 @@ INSERT INTO template (_id, jsonb) VALUES
      "body": "Dear {{user.firstName}},\n\nYour password has been changed.\nThis is a confirmation that your password was changed on {{dateTime}}.\n\nDid not change your password? Contact your Folio System Administrator to help secure your account.\n\t\nRegards,\n\nFolio Support"
    }
  }
+}'),
+('263d4e33-db8d-4e07-9060-11f442320c05', '{
+ "id": "263d4e33-db8d-4e07-9060-11f442320c05",
+ "description": "Account activation email",
+ "outputFormats": [
+   "text/plain"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Activate your Folio account",
+     "body": "{{user.personal.firstName}}\n\nYour Folio account has been activated. Your username is {{user.username}}. To complete activation of your account, please use the following link to create a password for your Folio account: {{link}}\n\nIf you do not create a password within 24 hours of the delivery of this email, then contact your Folio Administrator to receive a new create password link.\n\nRegards,\n\n{{institution.name}} Folio Administration"
+   }
+ }
+}'),
+('ed8c1c67-897b-4a23-a702-c36e280c6a93', '{
+ "id": "ed8c1c67-897b-4a23-a702-c36e280c6a93",
+ "description": "Rest password email",
+ "outputFormats": [
+   "text/plain"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Reset your Folio account",
+     "body": "{{user.personal.firstName}}\n\nYour Folio password has been reset. Please use this link to reset your password: {{link}}\n\nIf you do not reset your password within 24 hours of the delivery of this email, then contact your Folio Administrator to reset your password.\n\nRegards,\n\n{{institution.name}} Folio Administration"
+   }
+ }
 }') ON CONFLICT DO NOTHING;
