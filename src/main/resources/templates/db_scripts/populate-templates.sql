3bb65450ab081917013837309d624864ed65d7d9
Dmytro Tur <dmytro_tur@epam.com>
2018-11-28 11:19:35 +0200

    UIU-737 UIU-738 "Password is successfully created email" and "Username located email" templates added to pre-populate script

--- src/main/resources/templates/db_scripts/populate-templates.sql
+++ src/main/resources/templates/db_scripts/populate-templates.sql
@@ -40,4 +40,47 @@ INSERT INTO template (_id, jsonb) VALUES
      "body": "{{user.personal.firstName}}\n\nYour Folio password has been reset. Please use this link to reset your password: {{link}}\n\nIf you do not reset your password within 24 hours of the delivery of this email, then contact your Folio Administrator to reset your password.\n\nRegards,\n\n{{institution.name}} Folio Administration"
    }
  }
+}'),
+('ce9e3e2c-669a-4491-a12f-e0fdad066191', '{
+ "id": "ce9e3e2c-669a-4491-a12f-e0fdad066191",
+ "description": "Password is successfully created email template",
+ "outputFormats": [
+   "text/plain"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Folio account successfully created",
+     "body": "{{user.personal.firstName}}\n\nYour Folio account has been successfully created. You should now have access to Folio. \n\nShould you have any questions or you have received this email in error, please contact your Folio Administrator. \n\nRegards, \n\n{{institution.name}} Folio Administration"
+   }
+ }
+}'),
+('d0ee371a-f3f7-407a-b6f3-714362db6240', '{
+ "id": "d0ee371a-f3f7-407a-b6f3-714362db6240",
+ "description": "Username located email template",
+ "outputFormats": [
+   "text/plain"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Folio account located",
+     "body": "{{user.personal.firstName}}\n\nYour Folio username is {{user.username}}.\n\nPlease contact your Folio Administrator if you have any questions.\n\nRegards,\n\n{{institution.name}} Folio Administration"
+   }
+ }
 }') ON CONFLICT DO NOTHING;
+UPDATE "template" SET jsonb= '{
+ "id": "0ff6678f-53cd-4a32-9937-504c28f14077",
+ "description": "Template for password changed email",
+ "outputFormats": [
+   "text/plain"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Your Folio password changed",
+     "body": "Dear {{user.personal.firstName}},\n\nYour password has been changed.\nThis is a confirmation that your password was changed on {{dateTime}}.\n\nDid not change your password? Contact your Folio System Administrator to help secure your account.\n\t\nRegards,\n\nFolio Support"
+   }
+ }
+}'
+WHERE _id = '0ff6678f-53cd-4a32-9937-504c28f14077';
