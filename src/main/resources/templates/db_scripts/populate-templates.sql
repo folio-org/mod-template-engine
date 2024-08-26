058ef87bc42136e3fbb0626c0fbf622f03204dbc
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-08-26 10:11:38 +0530

    MODTEMPENG-105 modified templates of Account activation email and Reset password email

--- src/main/resources/templates/db_scripts/populate-templates.sql
+++ src/main/resources/templates/db_scripts/populate-templates.sql
@@ -95,8 +95,24 @@ UPDATE "template" SET jsonb= '{
  "localizedTemplates": {
    "en": {
      "header": "Complete activation of your FOLIO account",
-     "body": "<p>{{user.personal.firstName}}</p><p>Your FOLIO account has  activated.</p><p>Your username is {{user.username}}.</p><p>To complete activation of your account, please use the following link to create a password for your FOLIO account: <a href={{link}}>visit this link</a></p><p>If you do not create a password within {{expirationTime}} {{expirationUnitOfTime}} of the delivery of this email, then contact your FOLIO Administrator to receive a new create password link.</p><p>Regards,</p><p>{{institution.name}} FOLIO Administration</p>"
+     "body": "<p>{{user.personal.firstName}}</p><p>Your FOLIO account has been activated.</p><p>Your username is {{user.username}}.</p><p>To complete activation of your account, please use the following link to create a password for your FOLIO account: <a href={{link}}>visit this link</a></p><p>This link is only active for a short time. If the link has already expired, please <a href={{forgotPasswordLink}}>visit this link</a> to get a new, active link.</p><p>Regards,</p><p>{{institution.name}} FOLIO Administration</p>"
    }
  }
 }'
 WHERE id = '263d4e33-db8d-4e07-9060-11f442320c05';
+
+UPDATE "template" SET jsonb= '{
+ "id": "ed8c1c67-897b-4a23-a702-c36e280c6a93",
+ "description": "Reset password email",
+ "outputFormats": [
+   "text/html"
+ ],
+ "templateResolver": "mustache",
+ "localizedTemplates": {
+   "en": {
+     "header": "Reset your Folio account",
+     "body": "<p>{{user.personal.firstName}}</p><p>Your request to reset your password has been received.</p> <p>To reset your password, please <a href={{link}}>visit this link</a>.</p><p>NOTE: This link is only active for a short time. If the link has already expired, please <a href={{forgotPasswordLink}}>visit this link</a> to get a new, active link.</p><p>Regards,</p><p>{{institution.name}} FOLIO Administration</p>"
+   }
+ }
+}'
+WHERE id = 'ed8c1c67-897b-4a23-a702-c36e280c6a93';
