507d1e13e3f433d8e6eb60e3184b69525ae6a524
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-08-28 22:10:21 +0530

    MODTEMPENG-105 added templates to different files

--- src/main/resources/templates/db_scripts/account-activation-email-template.sql
+++ src/main/resources/templates/db_scripts/account-activation-email-template.sql
@@ -7,7 +7,7 @@ UPDATE "template" SET jsonb= '{
  "templateResolver": "mustache",
  "localizedTemplates": {
    "en": {
-     "header": "Complete activation of your Folio account",
+     "header": "Complete activation of your FOLIO account",
      "body": "<p>{{user.personal.firstName}}</p><p>Your FOLIO account has been activated.</p><p>Your username is {{user.username}}.</p><p>To complete activation of your account, please use the following link to create a password for your FOLIO account: <a href={{link}}>visit this link</a></p><p>This link is only active for a short time. If the link has already expired, please <a href={{forgotPasswordLink}}>visit this link</a> to get a new, active link.</p><p>Regards,</p><p>{{institution.name}} FOLIO Administration</p>"
    }
  }
--- src/main/resources/templates/db_scripts/reset-password-email-template.sql
+++ src/main/resources/templates/db_scripts/reset-password-email-template.sql
@@ -7,7 +7,7 @@ UPDATE "template" SET jsonb= '{
  "templateResolver": "mustache",
  "localizedTemplates": {
    "en": {
-     "header": "Reset your Folio account",
+     "header": "Reset your FOLIO account",
      "body": "<p>{{user.personal.firstName}}</p><p>Your request to reset your password has been received.</p> <p>To reset your password, please <a href={{link}}>visit this link</a>.</p><p>NOTE: This link is only active for a short time. If the link has already expired, please <a href={{forgotPasswordLink}}>visit this link</a> to get a new, active link.</p><p>Regards,</p><p>{{institution.name}} FOLIO Administration</p>"
    }
  }
