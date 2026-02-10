32fc0dd5261463a96bc34b2bedf498e4a2290645
Pavel Filippov <pavel_filippov@epam.com>
2026-02-10 15:58:48 +0200

    MODTEMPENG-127 Use more specific template instead of updating all

--- src/main/resources/templates/db_scripts/update-dateTime-in-password-changed-template.sql
+++ src/main/resources/templates/db_scripts/update-dateTime-in-password-changed-template.sql
@@ -1,2 +1,2 @@
 UPDATE "template" SET jsonb = replace(jsonb::text, '{{dateTime}}', '{{detailedDateTime}}')::jsonb
-WHERE jsonb::text LIKE '%{{dateTime}}%';
+WHERE id = '0ff6678f-53cd-4a32-9937-504c28f14077' AND jsonb::text LIKE '%{{dateTime}}%';
